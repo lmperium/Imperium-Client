38ebd77362e7a33104e549d4a6eca224aa7bd1d1
krekel <luisg.diazpr@gmail.com>
2019-03-15 16:57:22 -0500

    Moved comms package inside client directory.

---
+++ client/comms/HTTPWorker.py
@@ -0,0 +1,15 @@
+
+
+class HTTPWorker:
+    """This class serves as a wrapper for API calls to the server."""
+    def __init__(self):
+        self.url = ''
+
+    def register_worker(self):
+        pass
+
+    def upload_results(self):
+        pass
+
+    def respond_heartbeat(self):
+        pass
