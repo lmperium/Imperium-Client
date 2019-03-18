a98663c8ed53ea25a2fa97e1c65250b0eead6011
krekel <luisg.diazpr@gmail.com>
2019-03-18 15:19:05 -0500

    Implemented initial boot configuration. Worker registers with the API.

--- client/comms/HTTPWorker.py
+++ client/comms/HTTPWorker.py
@@ -1,12 +1,28 @@
+import aiohttp
+import asyncio
+import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HTTPWorker:
     """This class serves as a wrapper for API calls to the server."""
     def __init__(self):
-        self.url = ''
+        self.base_url = 'http://192.168.0.19:5000/api/'
 
-    def register_worker(self):
-        pass
+    async def register_worker(self, payload):
+        url = self.base_url + 'workers'
+
+        data = json.dumps(payload)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=url, data=data) as resp:
+                logger.info(resp.status)
+                response = await resp.json()
+                logger.info(response)
+
+        return response
 
     def upload_results(self):
         pass
--- client/worker.py
+++ client/worker.py
@@ -1,14 +1,15 @@
 import asyncio
 import logging
+import modules.system_information.win_system_information as sys_info
 import os
 import yaml
 
-from modules.system_information import win_system_information as sys_info
+from comms.HTTPWorker import HTTPWorker
 
 
 async def main():
     # Load configuration file as a python dictionary
-    abs_path = os.path.dirname(os.path.dirname(os.getcwd()))
+    abs_path = os.path.dirname(os.getcwd())
     file_path = abs_path + '\\config.yaml'
 
     with open(file_path, 'r') as stream:
@@ -22,17 +23,27 @@ async def main():
 
         init_info = dict()
         system_info = win_info.get_system_information()
+        logger.info('Retrieved system information')
 
         # Prepare payload
         init_info['hostname'] = system_info['node']
         init_info['startup_info'] = system_info
 
         # Send init_info to api and register worker
-
-        logger.info(init_info)
-
-        logger.info('Retrieved system information')
         # Register worker with the API.
+        http_worker = HTTPWorker()
+
+        target_queue = await http_worker.register_worker(payload=init_info)
+        logger.info(f'Received target queue: {target_queue["target_queue"]}')
+
+        with open(file_path) as stream:
+            config = yaml.safe_load(stream)
+            config['target_queue'] = target_queue['target_queue']
+
+        with open(file_path, 'w') as stream:
+            yaml.dump(config, stream)
+            logger.info(f'Added target queue {target_queue["target_queue"]} to config file')
+
     else:
         logger.info('Has been booted')
 
@@ -42,4 +53,4 @@ if __name__ == '__main__':
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    asyncio.run(main(), debug=True)
+    asyncio.run(main())
